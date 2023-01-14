12a0327a16c177397376e0775fb8c2a7b7811c78
Mandip Nijor <mandip.nijor@gmail.com>
2023-01-14 13:27:52 +0000

    initial variables created

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+// Initial Variables
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentWindSpeed = $("#current-wind-speed");
+var currentHumidity = $("#current-humidity");
+
+var weatherContent = $("#weather-content");
+

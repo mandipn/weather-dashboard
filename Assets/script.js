87a8d8c0ac7b9d5148ff2d972a349a4dcd538a61
Mandip Nijor <mandip.nijor@gmail.com>
2023-01-14 13:34:18 +0000

    adding api key citylist var current date and time and clearing history code

--- Assets/script.js
+++ Assets/script.js
@@ -11,3 +11,16 @@ var currentHumidity = $("#current-humidity");
 
 var weatherContent = $("#weather-content");
 
+// Get access to the OpenWeather API
+var APIkey = "a17e1499228be1f9c294ac18b234c7d7";
+
+// Easy access to data
+var cityList = [];
+
+// Find current date and display in title
+var currentDate = moment().format('L');
+$("#current-date").text("(" + currentDate + ")");
+
+// Check if search history exists when page loads
+initalizeHistory();
+showClear();
